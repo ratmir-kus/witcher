436112ce619206041fa2280955dc15fe9e0dadd6
ratmir-kus <72627773+ratmir-kus@users.noreply.github.com>
2020-10-09 21:28:43 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidePerView: 2,
+    loop: true,
+  	 navigation: {
+    nextEl: '.arrow',
+  },
+});
---
+++ js/var mySwiper = new Swiper('.swiper-container', {.js
@@ -0,0 +1,6 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidePerView: 2,
+    navigation: {
+    	nexEl: '.arrow',
+    }
+});
